4ad03790f6b28dab9e406b11cfb4fe65f503f6f7
Abhinav MS <abhinavms@outlook.com>
2020-10-20 13:54:07 +0530

    Added custom user model

--- main/settings.py
+++ main/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    # Apps
     'users',
     'clues',
 ]
@@ -118,6 +116,7 @@ USE_L10N = True
 
 USE_TZ = True
 
+AUTH_USER_MODEL = 'users.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
