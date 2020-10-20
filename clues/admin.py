814d82f387726443e0e9a16e4250769a902931af
Abhinav MS <abhinavms@outlook.com>
2020-10-20 16:28:54 +0530

    Added Level Model

--- clues/admin.py
+++ clues/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
-
+from .models import Level
 # Register your models here.
+
+class LevelAdmin(admin.ModelAdmin):
+    model = Level
+    list_display = ('no', 'text', 'picture', 'hiddenHTML', 'answer')
+    # fieldsets = (
+    #     (None, {'fields': ('email', 'password', 'level')}),
+    #     ('Permissions', {'fields': ('is_staff', 'is_active')}),
+    # )
+    # add_fieldsets = (
+    #     (None, {
+    #         'classes': ('wide',),
+    #         'fields': ('email', 'password1', 'password2', 'is_staff', 'is_active')}
+    #     ),
+    # )
+    search_fields = ('no',)
+    ordering = ('no',)
+
+admin.site.register(Level, LevelAdmin)
--- clues/models.py
+++ clues/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Level(models.Model):
+    no = models.IntegerField(default=0)
+    text = models.CharField(max_length=1500, blank=True)
+    picture = models.ImageField(upload_to='clues', default='', blank=True)
+    hiddenHTML = models.CharField(max_length=1500, blank=True)
+    answer = models.CharField(max_length=150)
